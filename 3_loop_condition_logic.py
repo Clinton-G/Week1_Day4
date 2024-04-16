9cf1e5f2db4961b5b63fa44a25b05b3b17a15ece
Clinton.G <clintongoin@gmail.com>
2024-04-15 20:17:02 -0500

    day4

---
+++ 3_loop_condition_logic.py
@@ -0,0 +1,26 @@
+# Task 1:
+
+
+
+
+tacos = []
+people = input('what is your name? ')
+
+
+
+while len(tacos) < 5:
+    print('handing out tacos to', people)
+    tacos.append(people)
+
+
+print(tacos)
+
+
+#----------------------------------------------------------------------------------------------------
+# Task 2:
+
+
+
+
+
+
---
+++ 4_python_game_night.py
@@ -0,0 +1,31 @@
+# Task 1:
+import random
+
+
+#   Create a game where a player has a list of items
+#   They have to guess which item in the list was selected. 
+#   Use random.choice() to select the item and take the user's guess via input.
+#   Provide feedback on whether they guessed correctly or not keep them playing until they guess correctly.
+
+card = input('pick a card type: ')
+type = ('Hearts', 'Spade', 'Clubs', 'Diamond ')
+
+while True:
+    my_choice = card
+    dealer = random.choice(type)
+    print('Dealer Chose:', dealer)
+    win = input('Did You Beat the House? ')
+    if win == 'yes':
+        print('The House Always Wins, Nice Try')
+        dealer = random.choice(type)
+        break
+    if win == 'no':
+        print('Play Again!')
+        dealer = random.choice(type)
+
+
+
+
+
+
+# random.choice()
