86b1e359da734ecfd2212d6f7c9d415f983bad23
Clinton.G <clintongoin@gmail.com>
2024-05-29 18:26:46 -0500

    Week1_Day4

--- 2_nested_loops.py
+++ 2_nested_loops.py
@@ -7,14 +7,13 @@ breakfast = ['bacon', 'waffle', 'poptart', 'toast', 'eggs']
 lunch = ['sandwhich', 'pasta', 'soup', 'mac n cheese', 'pho']
 dinner = ['chicken', 'wings', 'burger', 'ribs', 'carne asada']
 
-<<<<<<< HEAD
 for day in days:
     print("On", day, "I'll Be Having:")
     print(random.choice(breakfast), "For Breakfast")
     print(random.choice(lunch), "For Lunch")
     print("And", random.choice(dinner), "For Dinner")
     print("----------")
-=======
+
 for eachday in days:
     print(eachday, 'im going to have', random.choice(breakfast))
 
@@ -23,4 +22,3 @@ for eachday in days:
 
 for eachday in days:
     print(eachday, 'im going to have', random.choice(dinner))
->>>>>>> 660db2726e18c5e06fac4accb31973d3d52325c1
